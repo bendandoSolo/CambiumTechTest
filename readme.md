8e8ca7f4786ab46a6549517ecde2a9f1315966a8
ben roberts <bendando@hotmail.co.uk>
2022-05-18 14:16:10 +0100

    test showing on map

--- readme.md
+++ readme.md
@@ -10,7 +10,24 @@ Cannot find module 'babel-eslint'
 was solved with
 npm install babel-eslint --save-dev
 
-### Steps to Create Mars Rover Tech Test
+
+### Additional Refinements
+- Create proper error testing for malformed csv files
+- Create reset button to retry the test
+- Improve grid styling, prevent resizing cells
+- Save CSVs to Azure storage
+- Save tests to state management, so can be rerun
+- Add manual mode to move rovers around with keyboard
+- Save movements from manual mode into csv
+- Finish animations, add step forward and step back through test
+
+
+### Steps to Creating Mars Rover Tech Test
 - Created Project Frontend Structure
 - Create Grid to display results
 - Create CSV File Upload System
+- Process csv File into list of co-ordinates and directions
+- Add tests around the processing
+- Return data to Frontend
+- Display results in the front end
+- Display result in frontend grid
--- rover.spa/ClientApp/src/components/MarsMap.js
+++ rover.spa/ClientApp/src/components/MarsMap.js
@@ -1,16 +1,18 @@
 ﻿import React, { useState } from 'react';
 
+const rover = (index, direction, step) => ({ no: index, direction, step });
+//const MapCell = ({ data }) => <td>Rovers: {JSON.stringify(data.rovers)}</td>;
+const MapCell = ({ data }) => <td>{data.rovers.length ? JSON.stringify(data.rovers) : ' '}</td>;
 
 
 const MarsMap = ({data}) => {
-
     //create map array from 0,0 to 5,5
     const initializeEmptyMap = () => {
         let dataArray = [];
         for (var y = 0; y < 6; y++) {
             dataArray[y] = [];
             for (var x = 0; x < 6; x++) {
-                dataArray[y].push(' ');
+                dataArray[y].push({ rovers: [] });
             }
         }
         console.log(dataArray);
@@ -18,8 +20,24 @@ const MarsMap = ({data}) => {
     }
 
     const [mapArray, setMapArray] = useState(initializeEmptyMap());
+    const [step, setStep] = useState(0);
 
-
+    const runTest = () => {
+        let step = 0;
+        for (let i = 0; i < data.length; i++) {
+            let currentRoverData = data[i];
+            for (let j = 0; j < currentRoverData.length; j++) {
+                let xpos = currentRoverData[j].x;
+                let ypos = currentRoverData[j].y;
+                console.log(xpos + "" + ypos);
+                step++;
+                mapArray[ypos][xpos].rovers.push(rover(i, currentRoverData[j].direction, step));
+            }
+        }
+        setStep(step);
+        console.log(JSON.stringify(mapArray));
+        setMapArray([...mapArray]);
+    }
 
 
     return (
@@ -27,9 +45,16 @@ const MarsMap = ({data}) => {
             <h2>Map of area of mars that rovers will be traversing</h2>
             <table>
                 <tbody>
-                    {mapArray.map((row, i) => <tr key={i}>{row.map((x, i2) => <td key={i2}>{row[i]}</td>)}</tr>)}
+                    {mapArray.map((row, i) => <tr key={i}>{row.map((x, i2) => <MapCell key={i2} data={x} step={step} />)}</tr>)}
                 </tbody>
             </table>
+
+            {Object.keys(data).length > 0 &&
+                <>
+                <h3>Test Controls</h3>
+                <button onClick={runTest}>Run Test</button>
+                </>
+            }
             <h3>Current Map Data</h3>
             {mapArray.map(function (item, index) {
                 return (
