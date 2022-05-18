8fac689c04f4c6f9fdeb1ba1ecb45e4da71711c4
ben roberts <bendando@hotmail.co.uk>
2022-05-18 12:01:52 +0100

    created frontend csv uploader

--- rover.spa/ClientApp/src/components/CSVUploader.js
+++ rover.spa/ClientApp/src/components/CSVUploader.js
@@ -1,12 +1,46 @@
-﻿import React from 'react';
-
+﻿import React, {useCallback} from 'react';
+import { useDropzone } from 'react-dropzone';
 
 
 const CVSUploader = () => {
 
+    const onDrop = useCallback((acceptedFiles) => {
+        acceptedFiles.forEach((file) => {
+            const reader = new FileReader()
+            reader.onabort = () => console.log('file reading was aborted')
+            reader.onerror = () => console.log('file reading has failed')
+            reader.onload = () => {
+                var data = new FormData();
+                data.append('file', file);
+                uploadCSVToServer(data);
+            }
+            reader.readAsArrayBuffer(file)
+        })
+
+    }, [])
+
+    const uploadCSVToServer = async (filedata) => {
+        alert('we want to upload to server');
+
+    };
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
     return (
         <>
-        <h2>To begin upload a file to the server for processing</h2>
+            <h2>To begin upload a file to the server for processing</h2>
+            <div className='csv-uploader-container' {...getRootProps()}>
+                <input {...getInputProps()} />
+                {
+                    isDragActive ?
+                        <p>Drop the files here ...</p> :
+                        isDragActive ? <p>Drop files here...</p> :
+                            <div>
+                                <h5 className='csv-uploader-header' >Upload CSV File</h5>
+                                <p className='text-center'>Try dropping a CSV file here, or click to select files to upload.</p>
+                            </div>
+                }
+            </div>
         </>
         )
 
